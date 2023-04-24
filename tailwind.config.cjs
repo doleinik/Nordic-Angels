1aab8c846b75eb62bf2403cb598205424bda93b8
vladislavslobodanuk <vladslobodanuk9@gmail.com>
2023-04-24 17:35:19 +0300

    add section podcast

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -22,7 +22,7 @@ module.exports = {
             },
         },
         fontFamily: {
-            'font': 'var(--font)'
+            'font-circular': 'var(--font-circular)',
         },
         extend: {
             colors: {
